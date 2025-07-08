c5752449a3422679bee719d5135a1823d35b639c
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2025-07-08 23:48:53 +0700

    chore: fix size wallet card

--- src/pages/user/Portfolio.tsx
+++ src/pages/user/Portfolio.tsx
@@ -135,7 +135,7 @@ const Portfolio = () => {
           Danh mục cổ phiếu
         </h2>
         {wallet && wallet.length > 0 ? (
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
             {wallet.map((item) => (
               <WalletCard key={item.id} item={item} onBuy={handleBuyStock} onSell={handleSellStock} />
             ))}
